110f72dcd62e1e1e49527c330af0ceea3d31f43d
sushant12811 <steve.malistella@gmail.com>
2025-04-15 19:39:45 -0400

    added set

---
+++ MyPlayground.playground/Pages/17. Sets.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//MARK: Sets
+/*
+ Swift sets are used to store distinct values(unique) of the same types
+ but they dont have definite ordering as arrays have(random order).
+ */
+
+//Syntax
+var set1: Set<Int> = [1, 2, 3, 4, 5]
+//or
+var set2: Set = ["red", "green", "blue"]
+
+print(set1)
